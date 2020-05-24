dda6411c548f5b069da039e570389b7a58210862
ashwindasr <ashwindasr@gmail.com>
2020-05-25 02:33:50 +0530

    Updated v1.0

--- v1.0/python/app.py
+++ v1.0/python/app.py
@@ -9,7 +9,7 @@ def add_anime(anime_name, episodes, current_episode_=0):
     Args:
         anime_name (str): Name of the anime.
         episodes (int): Total episodes in the anime.
-        current_episode_ (int) : Current episode count
+        current_episode_ (int) : Current episode count. Default is 0 for new anime.
     Returns:
         None
     """
@@ -28,11 +28,6 @@ def anime_exists(anime_name):
         anime_name (str): Name of the anime.
     Returns:
         bool: True if the anime exists else False.
-    Doctest:
-        >>> anime_exists("Tower of God")
-        True
-        >>> anime_exists("Apple")
-        False
     """
 
     with open(DATABASE_PATH, "r") as file:
@@ -52,15 +47,14 @@ def update_anime(anime_name, episode):
     Returns:
         None
     """
-    anime_data = []
+    anime_data = []  # Necessary line
     with open(DATABASE_PATH, "r") as file:
         anime_data = file.readlines()
 
-    # for idx, anime_ in enumerate(anime_data):
-    #     print(f"{idx + 1}. {anime_[0]} {anime_[]}/{anime_[2]}")
     for idx, anime_ in enumerate(anime_data):
         line_list = anime_.split("~")
         if anime_name in line_list[0]:
+            anime_full_name = line_list[0]
             anime_data[idx] = f"{line_list[0]}~{episode}~{line_list[2]}"
             break
 
@@ -68,7 +62,7 @@ def update_anime(anime_name, episode):
     with open(DATABASE_PATH, "w") as file:
         file.writelines(anime_data)
 
-    print(f"\nSuccess! Updated the episode count!\n\n")
+    print(f"\nSuccess! Updated the episode count of {anime_full_name}!\n\n")
 
 
 def anime_progress(anime_name):
@@ -79,19 +73,14 @@ def anime_progress(anime_name):
     Args:
           anime_name (str): Name of the anime.
     Returns:
-        (tuple): (Current episode, Total episodes)
-    Doctest:
-        >>> anime_progress("Tower of God")
-        ('0', '12')
-        >>> anime_progress("Gintama")
-        ('3', '12')
+        (tuple): (Full name of anime, Current episode, Total episodes)
     """
 
     with open(DATABASE_PATH, "r") as file:
         for line in file:
             line_list = line.split("~")
             if anime_name in line_list[0]:
-                return tuple([line_list[1], line_list[2].rstrip("\n")])
+                return tuple([line_list[0], line_list[1], line_list[2].rstrip("\n")])
 
 
 def anime_progress_all():
@@ -123,26 +112,27 @@ What do you want to do?
 Enter your choice:> """)
 
         if choice == "1":
-            new_anime = input("Enter the name of the anime:> ")
+            anime = input("Enter the name of the anime:> ")
             current_episode = int(input("Enter the current episode number:> "))
             total_episodes = int(input("Enter the total number of episodes:> "))
 
-            if not anime_exists(new_anime):
-                add_anime(new_anime, total_episodes, current_episode)
+            if not anime_exists(anime):
+                add_anime(anime, total_episodes, current_episode)
             else:
                 print("Anime already exists")
         elif choice == "2":
-            anime_input = input("Enter the name of the anime:> ")
-            if anime_exists(anime_input):
+            anime = input("Enter the name of the anime:> ")
+            if anime_exists(anime):
                 current_episode = int(input("Enter the current episode number:> "))
-                update_anime(anime_input, current_episode)
+                update_anime(anime, current_episode)
             else:
                 print("\nThe anime does not exist.\n")
         elif choice == "3":
-            anime_input = input("Enter the name of the anime:> ")
-            if anime_exists(anime_input):
-                current, total = anime_progress(anime_input)
-                print(f"{anime_input}: {current}/{total}\n")
+            anime = input("Enter the name of the anime:> ")
+            if anime_exists(anime):
+                anime_full_name, current, total = anime_progress(anime)
+                print("\nProgress:")
+                print(f"{anime_full_name}: {current}/{total}\n")
             else:
                 print("\nThe anime does not exist.\n")
         elif choice == "4":
@@ -161,8 +151,6 @@ Enter your choice:> """)
 
 
 if __name__ == '__main__':
-    # import doctest
-    # doctest.testmod()
     print("Current Watch list:\n")
     all_anime = anime_progress_all()
     if all_anime == ():
