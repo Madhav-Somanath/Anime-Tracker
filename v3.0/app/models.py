1eac473b04bbb4c4ff9b0df1edc28f4f3769cdb7
Ashwin Das <ashwindasr@gmail.com>
2020-09-21 19:16:20 +0530

    error

--- v3.0/app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class AnimeModel(models.Model):
-    name = models.CharField(max_length=100)
-    current_episode = models.IntegerField(default=0)
-    total_episodes = models.IntegerField()
--- v3.0/app/static/js/progress.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var progressUrl = "{% url 'celery_progress:task_status' task_id %}";
-    CeleryProgressBar.initProgressBar(progressUrl);
-});
